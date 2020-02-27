40aece7abb5465346c7f67f75f4cda5c0b6a26c4
Yusheng Zhou 周宇盛 <jasonzhouu@outlook.com>
2020-02-27 16:03:58 +0800

    更改添加新行的样式

--- popover.js
+++ popover.js
@@ -1,18 +1,21 @@
+let option = {
+    content: "",
+    trigger: "hover",
+    delay: 200,
+    placement: "auto",
+}
+
 $("#audioController").popover({
+    ...option,
     content: "暂停/开始",
-    trigger: "hover",
-    delay: 200,
-    placement: "bottom",
 })
-$("#start").popover({
-    content: "获取这个句子开头的时间",
-    trigger: "hover",
-    delay: 200,
-    placement: "bottom",
+
+$("#addSlice").popover({
+    ...option,
+    content: "添加新句子",
 })
-$("#end").popover({
-    content: "获取这个句子结尾的时间",
-    trigger: "hover",
-    delay: 200,
-    placement: "bottom",
+
+$("#saveToJsonFile").popover({
+    ...option,
+    content: "将字幕保存到JSON文件",
 })
