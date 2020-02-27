be72a233b22b76edc6945c5b1e33c0176d3ac32f
Yusheng Zhou 周宇盛 <jasonzhouu@outlook.com>
2020-02-27 13:28:21 +0800

    初始化timeSlice

--- timeSlice.js
+++ timeSlice.js
@@ -2,12 +2,22 @@ import {wavesurfer} from './wavesurfer.js'
 
 // 一个数组，保存着所有句子的数据
 var timeSlice = [];
-// 一个对象，保存当前句子的数据。提交时，将其保存到timeSlice数组，并清空currentSlice对象。
 var currentSlice = {};
+// 一个对象，保存当前句子的数据。提交时，将其保存到timeSlice数组，并清空currentSlice对象。
+function addNewSlice() {
+    timeSlice.push({
+        start: null,
+        end: null,
+        note: null,
+    });
+    currentSlice = timeSlice[timeSlice.length-1]
+}
+addNewSlice();
+
 
 function addNewRow() {
     var newRow = $('<tr class="editableRow"></tr>').append(
-        $("<th></th>").html(timeSlice.length + 1)
+        $("<th></th>").html(timeSlice.length)
     ).append(
         $("<td></td>")
     ).append(
@@ -33,11 +43,9 @@ $("#addSlice").click(function(){
     $("#timeSliceTable tbody tr:last").removeClass("editableRow")
     $("#timeSliceTable tbody tr:last td").off("click")
     // @todo: 检查currentSlice是否符合要求，各属性不能为空
-    timeSlice.push({ ...currentSlice })
-    currentSlice = {}
-    console.log(timeSlice);
+    addNewSlice()
     addNewRow()
 })
 
 
-export {timeSlice, currentSlice}
+export {timeSlice}
