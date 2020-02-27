808b401b5adb6cc87607d1c2241ae3734b20136f
Yusheng Zhou 周宇盛 <jasonzhouu@outlook.com>
2020-02-27 09:20:29 +0800

    -

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ ipcMain.on('save-as-json-file', (event, arg) => {
 
 function saveJsonToFile(data) {
     console.log(data);
-    // 参考：
+    // 保存JSON格式的文件，需要先将对象转成JSON格式，使用函数JSON.stringify(data)。参考：
     // fs.writeFile(): https://mp.weixin.qq.com/s?src=11&timestamp=1582584858&ver=2179&signature=uOuh3gfkNBDT*Z7O1JwaaIB5YMc7wRcp-oPgsk-zC*ePU9I*BJ7bA5t1bkl14OawiGGigD1y*QjZF6k2SuwTAc4n9snuf5FLcc9pqXCA7E7uhV5Cma0l6E6vsI3Nc-*A&new=1
     fs.writeFile("xwz.json", JSON.stringify(data), (err)=>{
         if(err){
