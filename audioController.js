9b20dcf074d8afbc58dced5880f3d850c8efe0a6
Yusheng Zhou 周宇盛 <jasonzhouu@outlook.com>
2020-02-27 12:05:55 +0800

    addNewRow()

--- audioController.js
+++ audioController.js
@@ -1,5 +1,4 @@
 import {wavesurfer, setAudioController} from './wavesurfer.js'
-import {currentSlice} from './timeSlice.js'
 
 $("#audioController").on("click", function () {
     wavesurfer.playPause()
--- timeSlice.js
+++ timeSlice.js
@@ -1,12 +1,30 @@
+import {wavesurfer} from './wavesurfer.js'
+
 // 一个数组，保存着所有句子的数据
 var timeSlice = [];
 // 一个对象，保存当前句子的数据。提交时，将其保存到timeSlice数组，并清空currentSlice对象。
 var currentSlice = {};
 
+function addNewRow() {
+    $("#timeSliceTable tbody").append(
+        $("<tr></tr>").append(
+            $("<th>1</th>").html(timeSlice.length + 1)
+        ).append(
+            $("<td>2</td>")
+        ).append(
+            $("<td>3</td>")
+        ).append(
+            $("<td>4</td>")
+        )
+    )
+}
+addNewRow()
+
 $("#start").click(function () {
     currentSlice.start = wavesurfer.getCurrentTime();
     $("#start + span").html(currentSlice.start)
 })
+
 
 $("#end").click(function () {
     currentSlice.end = wavesurfer.getCurrentTime();
