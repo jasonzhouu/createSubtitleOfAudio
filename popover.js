4a6986e791149632374860e552990cc2a550d840
Yusheng Zhou 周宇盛 <jasonzhouu@outlook.com>
2020-02-27 17:53:12 +0800

    按钮样式

--- popover.js
+++ popover.js
@@ -9,11 +9,6 @@ $("#audioController").popover({
     content: "暂停/开始",
 })
 
-$("#addSlice").popover({
-    ...option,
-    content: "添加新句子",
-})
-
 $("#saveToJsonFile").popover({
     ...option,
     content: "将字幕保存到JSON文件",
