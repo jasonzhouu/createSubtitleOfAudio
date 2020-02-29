8ec5ff21869dbf1221c183abb68df30075d1ca63
Yusheng Zhou 周宇盛 <jasonzhouu@outlook.com>
2020-02-29 16:32:52 +0800

    npm init

--- main.js
+++ main.js
@@ -51,4 +51,4 @@ app.on('window-all-closed', function () {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') app.quit()
-  })
+})
