80484c1de2c1fe779744f765e534355856ab39eb
Yusheng Zhou 周宇盛 <jasonzhouu@outlook.com>
2020-02-29 17:01:40 +0800

    test Store in renderer process

--- timeSlice.js
+++ timeSlice.js
@@ -4,6 +4,13 @@ import {wavesurfer} from './wavesurfer.js'
 var timeSlice = [];
 var currentSlice = {};
 
+function addNewSliceAndRow() {
+    // 在timeSlice数组中添加新项
+    addNewSlice();
+    // 在DOM table中添加新的一行
+    addNewRow();
+}
+
 // 一个对象，保存当前句子的数据。提交时，将其保存到timeSlice数组，并清空currentSlice对象。
 function addNewSlice() {
     var temp = new Proxy({ // 如何监听 js 中变量的变化? https://www.zhihu.com/question/44724640/answer/117339055
@@ -28,8 +35,6 @@ function addNewSlice() {
     // 新建行时，也要disable button
     disableNewRowButton();
 }
-addNewSlice();
-
 
 function addNewRow() {
     var newRow = $('<tr class="editableRow"></tr>').append(
@@ -57,14 +62,8 @@ function addEventToNewRow(newRow) {
         currentSlice.note = $(this).html()
     })
 }
-addNewRow()
 
 $("#addSlice").click(function(){
-    // @todo: 检查currentSlice是否符合要求，各属性不能为空
-    if(!isCurrentRowIsFinished()) {
-        alert("Can't add a new row before current row is finished")
-        return;
-    }
     // editableRow class的样式不一样。添加后，应该把这个样式去掉。
     $("#timeSliceTable tbody tr:last").removeClass("editableRow")
     $("#timeSliceTable tbody tr:last td").off("click")
@@ -74,8 +73,8 @@ $("#addSlice").click(function(){
         var endTime = parseFloat($(this).children("td").eq(1).html())
         wavesurfer.play(startTime, endTime)
     })
-    addNewSlice()
-    addNewRow()
+    addNewSliceAndRow()
+    saveTimeSliceToElectronStore()
 })
 
 function isCurrentRowIsFinished() {
@@ -98,5 +97,16 @@ function disableNewRowButton() {
         $("button#addSlice").prop("disabled", false); 
     }
 }
+
+function saveTimeSliceToElectronStore() {
+    console.log('save to json');
+    const store = new Store()
+    store.set('hello', 'world')
+    console.log(store.get('hello'));
+    
+}
+
+addNewSliceAndRow()
+
 
 export {timeSlice}
