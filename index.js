702a353539f73c42f249ecc26f565b8bba8daa49
Yusheng Zhou 周宇盛 <jasonzhouu@outlook.com>
2020-03-01 11:28:28 +0800

    export json file

--- index.js
+++ index.js
@@ -15,3 +15,14 @@ $('#addSlice').click(function() {
 })
 
 // todo: 组织代码，存放在src目录
+
+
+$("#saveToJsonFile").on("click", function () {
+    // 将数据发送到主进程，消息类型：save-as-json-file
+    ipcRenderer.send('save-as-json-file', timeSlice)
+})
+
+// 接收主进程返回的消息
+ipcRenderer.on('save-file-finished', (event, arg) => {
+    console.log("save file: " + arg)
+})
