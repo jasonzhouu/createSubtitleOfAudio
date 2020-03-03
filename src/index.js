e4d66af99206feb2f8732952b2f4e68750a615f3
Yusheng Zhou 周宇盛 <jasonzhouu@outlook.com>
2020-03-03 20:12:27 +0800

    切换显示音频列表

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ const audioPage = new AudioPage()
 
 audioPage.show('xwz1')
 
-setTimeout(() => {
-    audioPage.show('xwz2')
-}, 2000);
+// setTimeout(() => {
+//     audioPage.show('xwz2')
+// }, 2000);
+
+$('#showAudioList').click(function() {
+    $('#audioList').show()
+    $('#audioPage').hide()
+})
+$('#closeAudioList').click(function() {
+    $('#audioList').hide()
+    $('#audioPage').show()
+})
