9f24da3747184aca01b08c49a261e8624cc37797
Yusheng Zhou 周宇盛 <jasonzhouu@outlook.com>
2020-03-03 17:52:45 +0800

    comment

--- src/index.js
+++ src/index.js
@@ -10,10 +10,14 @@ function AudioPage(audioName) {
     var timeSlice = new TimeSlice(audioName)
     
     this.show = function() {
+        // 显示音频波
         const audioWave = showAudioWave(audioName + '1.mp3')
+
+        // 显示表格
         var table = new Table(audioWave, timeSlice)
         table.show()
         
+        // 给按钮添加事件
         addEventToButtons(audioWave, timeSlice, table)
 
     }
