ecda8e002d6f88d8621c61a4aa3809d158067205
Yusheng Zhou 周宇盛 <jasonzhouu@outlook.com>
2020-03-04 16:35:18 +0800

    main.js 删除备注

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
 const path = require('path')
-const { app, BrowserWindow, ipcMain } = require('electron')
-const fs = require('fs')
+const { app, BrowserWindow } = require('electron')
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 1000,
         height: 800,
         webPreferences: {
-            // 参考 https://newsn.net/say/electron-require-not-defined.html
-            // nodeIntegration: true,
-            // 这样不安全，有可能会因为XSS攻击，使得用户的电脑受侵害。
-            // 更安全的做法参考 https://github.com/electron/electron/issues/9920#issuecomment-575839738
             nodeIntegration: false,
             preload: __dirname + '/preload.js'
         }
